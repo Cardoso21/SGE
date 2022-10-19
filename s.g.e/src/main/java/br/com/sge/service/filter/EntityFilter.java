0e03b1d4a5da3931a20b90771e4fd4b7632e8e56
Cardoso21 <wagner.cardoso20@gmail.com>
2022-10-19 14:18:04 -0300

    indo pra produçao

--- s.g.e/src/main/java/br/com/sge/service/filter/EntityFilter.java
+++
@@ -1,7 +0,0 @@
-package br.com.sge.service.filter;
-
-import org.springframework.data.jpa.domain.Specification;
-
-public interface EntityFilter<E> {
-    Specification<E> filtrar();
-}
--- s.g.e/src/main/java/br/com/sge/service/mapper/EventoListarMapper.java
+++
@@ -1,9 +0,0 @@
-package br.com.sge.service.mapper;
-
-import br.com.sge.dominio.Evento;
-import br.com.sge.service.dto.EventoListarDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", uses = {SituacaoSelectMapper.class})
-public interface EventoListarMapper extends EntityMapper<EventoListarDTO, Evento> {
-}
