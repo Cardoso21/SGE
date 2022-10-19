baeb2ceae71fe6d3ca472a013c712bf4d92ca81e
Cardoso21 <wagner.cardoso20@gmail.com>
2022-10-19 12:15:52 -0300

    atualizaçao

--- s.g.e/src/main/java/br/com/sge/repositorio/SituacaoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.sge.repositorio;
-
-import br.com.sge.dominio.Situacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SituacaoRepositorio extends JpaRepository<Situacao,Long> {
-}
--- s.g.e/src/main/java/br/com/sge/servico/filtro/EntityFiltro.java
+++
@@ -1,8 +0,0 @@
-package br.com.sge.servico.filtro;
-
-import org.springframework.data.jpa.domain.Specification;
-
-public interface EntityFiltro <T>{
-
-    Specification<T> filter();
-}
